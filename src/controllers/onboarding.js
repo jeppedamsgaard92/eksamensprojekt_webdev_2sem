6b78e3f6ffc64fe7e15145073124f3a695dfba6c
andersdamsgaard95 <145433759+andersdamsgaard95@users.noreply.github.com>
2026-05-20 12:52:11 +0200

    fejl fikset

--- src/controllers/onboarding.js
+++ src/controllers/onboarding.js
@@ -1,4 +1,4 @@
 
 export function uploadPdfFiles(files) {
-
+    return 'hej';
 }
--- src/routes/onboarding.js
+++ src/routes/onboarding.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { uploadPdfFiles } from '../controllers/onboarding';
+import { uploadPdfFiles } from '../controllers/onboarding.js';
 
 const router = express.Router();
 
-router.post('/pdf-slides', /* LoggetInd, requireAdmin */ uploadPdfFiles(files));
+router.post('/pdf-slides', /* LoggetInd, requireAdmin */ uploadPdfFiles);
 
 export default router;
 
