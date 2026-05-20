a06df807e3e8bbd939b570cdaa8922779562f38d
andersdamsgaard95 <145433759+andersdamsgaard95@users.noreply.github.com>
2026-05-20 12:41:42 +0200

    npm installed multer

--- src/routes/onboarding.js
+++ src/routes/onboarding.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import { uploadPdfFiles } from '../controllers/onboarding';
 
 const router = express.Router();
+
+router.post('/pdf-slides', /* LoggetInd, requireAdmin */ uploadPdfFiles(files));
 
 export default router;
 
