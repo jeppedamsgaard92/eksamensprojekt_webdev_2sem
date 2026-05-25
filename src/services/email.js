32fff6ed150b3e93d42c1f6c87ad4a249412fd4f
jeppedamsgaard92 <jeppedamsgaard92@gmail.com>
2026-05-25 14:03:13 +0200

    escaping af brugernavn i email, der sendes

--- src/services/email.js
+++ src/services/email.js
@@ -2,6 +2,15 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+function escapeHtml(value) {
+  return String(value)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
+
 export async function sendRegistrationInvitationEmail({ to, name, registrationLink }) {
   if (!process.env.RESEND_API_KEY) {
     throw new Error("RESEND_API_KEY mangler i .env");
@@ -11,12 +20,14 @@ export async function sendRegistrationInvitationEmail({ to, name, registrationLi
     throw new Error("EMAIL_FROM mangler i .env");
   }
 
+  const safeName = escapeHtml(name);
+
   const { data, error } = await resend.emails.send({
     from: process.env.EMAIL_FROM,
     to,
     subject: "Complete your registration",
     html: `
-      <p>Hi ${name},</p>
+      <p>Hi ${safeName},</p>
       <p>You have been invited to complete your registration.</p>
       <p>
         <a href="${registrationLink}">
@@ -27,7 +38,10 @@ export async function sendRegistrationInvitationEmail({ to, name, registrationLi
     `,
   });
 
+  console.log(registrationLink);
+
   if (error) {
+    console.error("Error sending email:", error);
     throw new Error(error.message);
   }
 
