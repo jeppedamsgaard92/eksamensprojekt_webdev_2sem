ef631b991badcbba7296fc3e68d498553e894dca
jeppedamsgaard92 <jeppedamsgaard92@gmail.com>
2026-05-25 12:46:45 +0200

    rettelse

--- src/routes/registrations.js
+++ src/routes/registrations.js
@@ -22,7 +22,7 @@ router.post("/", requirePendingRegistrationSession, requireCsrfToken, validateCo
 
 // admin til at oprette klient og admin accounts
 router.post(
-  "/create-new-client-account/:id",
+  "/create-new-client-account/:id?",
   requireAuth,
   requirePermission("create:client-account"),
   requireCsrfToken,
